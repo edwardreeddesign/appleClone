daca61776d2dbe2b6d2550a3cd1a2cdc9b69ee5f
Ed Reed <edreed@Eds-MacBook-Air.local>
2024-06-13 11:18:38 -0400

    added navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import Navbar from '@/components/Navbar';
 import Image from 'next/image';
 
 export default function Home() {
   return (
     <main className="bg-black">
       {/* Nav */}
+      <Navbar />
       {/* Hero */}
       {/* Highlights */}
       {/* Model */}
